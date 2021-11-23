d31db173174d762135481998e7fdf0ab4c3ebf35
Gosh95 <pocal777@naver.com>
2021-11-23 14:16:09 +0900

    포켓몬
    https://programmers.co.kr/learn/courses/30/lessons/1845

---
+++ src/test/programmers/level1/Pocketmon.java
@@ -0,0 +1,31 @@
+package test.programmers.level1;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Pocketmon {
+    public static void main(String[] args) {
+        int[] arr = {3, 3, 3, 2, 2, 4};
+        System.out.println(solution(arr));
+    }
+    static int solution(int[] nums) {
+        int answer = 0;
+        Set<Integer> set = new HashSet<>();
+
+        for(int i : nums) {
+            set.add(i);
+        }
+
+        int maxCount = nums.length / 2; //최대 고를 수 있는 갯수
+        int countOfTypes = set.size(); //종류의 갯수
+
+        if(countOfTypes >= maxCount) { //종류의 갯수가 고를 수 있는 갯수보다 많다
+            answer = maxCount;
+        } else {
+            answer = countOfTypes;
+        }
+
+        return answer;
+    }
+}
