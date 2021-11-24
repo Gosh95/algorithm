2031067c1caaa074394bfdd0efe2c839955d43eb
Gosh95 <pocal777@naver.com>
2021-11-24 18:11:45 +0900

    약수의 개수와 덧셈
    https://programmers.co.kr/learn/courses/30/lessons/77884

---
+++ src/test/programmers/level1/NumberAndAdditionOfFactors.java
@@ -0,0 +1,29 @@
+package test.programmers.level1;
+
+public class NumberAndAdditionOfFactors {
+    public static void main(String[] args) {
+        System.out.println(solution(13, 17));
+    }
+    static int solution(int left, int right) {
+        int answer = 0;
+
+        for(int i = left; i <= right; i++) {
+            int count = 0;
+
+            for(int j = 1; j <= i; j++) {
+
+                if(i % j == 0) {
+                   count++;
+                }
+            }
+
+            if(count % 2 == 0) {
+                answer += i;
+            } else {
+                answer -= i;
+            }
+        }
+
+        return answer;
+    }
+}
