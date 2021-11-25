add5b5c33186ae42715c8d29b3c3d300ab094c31
Gosh95 <pocal777@naver.com>
2021-11-25 13:50:00 +0900

    3진법 뒤집기
    https://programmers.co.kr/learn/courses/30/lessons/68935

---
+++ src/test/programmers/level1/TrinaryDigit.java
@@ -0,0 +1,32 @@
+package test.programmers.level1;
+
+import java.util.ArrayList;
+
+public class TrinaryDigit {
+    public static void main(String[] args) {
+        System.out.println(solution(125));
+    }
+
+    static int solution(int n) {
+        int answer = 0;
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        while(true) {
+            if(n < 3) {
+                arr.add(n);
+                break;
+            }
+
+            int a = n % 3;
+            arr.add(a);
+            n = n / 3;
+        }
+
+        for(int i = 0; i < arr.size(); i++) {
+            answer += arr.get(i) * Math.pow(3, arr.size() - 1 - i);
+        }
+
+        return answer;
+    }
+}
