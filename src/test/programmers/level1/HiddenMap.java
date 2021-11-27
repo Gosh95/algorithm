caaf852c9d63fa3b26dc461229360e86b9a5e535
Gosh95 <pocal777@naver.com>
2021-11-28 03:49:24 +0900

    [1차] 비밀지도
    https://programmers.co.kr/learn/courses/30/lessons/17681
    
    Integer.toBinaryString - 숫자를 2진수 문자열 형태로 바꿔준다.

---
+++ src/test/programmers/level1/HiddenMap.java
@@ -0,0 +1,35 @@
+package test.programmers.level1;
+
+import java.util.Arrays;
+
+public class HiddenMap {
+    public static void main(String[] args) {
+        int[] arr1 = {9, 20, 28, 18, 11};
+        int[] arr2 = {30, 1, 21, 17, 28};
+
+        Arrays.toString(solution(5, arr1, arr2));
+    }
+
+    static String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
+        answer = new String[n];
+
+        for(int i = 0; i < n; i++) {
+            String temp = "";
+            String binary1 = Integer.toBinaryString(arr1[i]);
+            while(binary1.length() < n) binary1 = "0" + binary1;
+
+            String binary2 = Integer.toBinaryString(arr2[i]);
+            while(binary2.length() < n) binary2 = "0" + binary2;
+
+            for(int j = 0; j < n; j++) {
+                temp += (binary1.charAt(j) - '0' + binary2.charAt(j) - '0' == 0) ? " " : "#";
+            }
+
+            answer[i] = temp;
+        }
+
+        return answer;
+    }
+
+}
