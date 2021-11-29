f6cd6e8f825a92deb975a478c7523b371772ee8f
Gosh95 <pocal777@naver.com>
2021-11-29 14:39:47 +0900

    같은 숫자는 싫어
    https://programmers.co.kr/learn/courses/30/lessons/12906
    
    배열 arr의 원소의 크기 : 0보다 크거나 같고 9보다 작거나 같은 정수
    keyPoint : 숫자의 크기가 0 ~ 9 이기 때문에 temp 변수에 10을 주어 비교를 하여 같지 않으면 숫자를 넣고 temp 를 넣은 숫자로 바꾼다.

---
+++ src/test/programmers/level1/HateSameNumber.java
@@ -0,0 +1,36 @@
+package test.programmers.level1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class HateSameNumber {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 3, 3, 0, 1, 1};
+
+        System.out.println(Arrays.toString(solution(arr)));
+    }
+
+    static int[] solution(int []arr) {
+        int[] answer = {};
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int temp = 10;
+
+        for(int num : arr) {
+            if(temp != num) {
+                list.add(num);
+                temp = num;
+            }
+        }
+
+        answer = new int[list.size()];
+
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+}
