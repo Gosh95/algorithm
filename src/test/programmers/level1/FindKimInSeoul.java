5375d8e86622457759c0db9578d2d7301213ef05
Gosh95 <pocal777@naver.com>
2021-12-02 10:09:22 +0900

    서울에서 김서방 찾기
    https://programmers.co.kr/learn/courses/30/lessons/12919

---
+++ src/test/programmers/level1/FindKimInSeoul.java
@@ -0,0 +1,22 @@
+package test.programmers.level1;
+
+public class FindKimInSeoul {
+    public static void main(String[] args) {
+        String[] ar = {"Jane", "Kim"};
+
+        System.out.println(solution(ar));
+    }
+
+    static String solution(String[] seoul) {
+        String answer = "";
+
+        for(int i = 0; i < seoul.length; i++) {
+            if(seoul[i].equalsIgnoreCase("kim")) {
+                answer = "김서방은 " +  i + "에 있다";
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
