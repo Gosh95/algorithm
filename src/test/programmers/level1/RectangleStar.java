3ec17f79a89b2b716f1e1f49bac00d72b13b09c6
Gosh95 <pocal777@naver.com>
2021-12-05 19:31:18 +0900

    직사각형 별찍기
    https://programmers.co.kr/learn/courses/30/lessons/12969
    
    다른사람 풀이 코드 : IntStream 이용해서 스트림으로도 가능

---
+++ src/test/programmers/level1/RectangleStar.java
@@ -0,0 +1,31 @@
+package test.programmers.level1;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class RectangleStar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        sb.append("*".repeat(a));
+
+        for(int i = 0; i < b; i++) {
+            System.out.println(sb.toString());
+        }
+
+      /* 다른사람 코드 : 스트림 사용해서도 가능
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+        IntStream.range(0, a).forEach(s -> sb.append("*"));
+        IntStream.range(0, b).forEach(s -> System.out.println(sb.toString()));
+        */
+
+    }
+}
