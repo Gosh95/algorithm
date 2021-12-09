022b1253cb04763c3a3922a05c19312ef2295c60
Gosh95 <pocal777@naver.com>
2021-12-09 08:14:50 +0300

    14681 사분면 고르기
    https://www.acmicpc.net/problem/14681

---
+++ src/test/baekjoon/if문/PickAQuadrant.java
@@ -0,0 +1,22 @@
+package test.baekjoon.if문;
+
+import java.util.Scanner;
+
+public class PickAQuadrant {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        if(x > 0) {
+            if(y > 0) System.out.println(1);
+            if(y < 0) System.out.println(4);
+        }
+
+        if(x < 0) {
+            if(y > 0) System.out.println(2);
+            if(y < 0) System.out.println(3);
+        }
+    }
+}
