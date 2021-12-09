599506cf676285456bac792adec97a28e5358764
Gosh95 <pocal777@naver.com>
2021-12-09 07:10:39 +0300

    피보나치 수
    https://programmers.co.kr/learn/courses/30/lessons/12945

---
+++ src/test/programmers/level2/FibonacciNumber.java
@@ -0,0 +1,25 @@
+package test.programmers.level2;
+
+public class FibonacciNumber {
+    public static void main(String[] args) {
+        System.out.println(solution(4));
+    }
+
+    static int solution(int n) {
+        int answer = 0;
+
+        int ppnum = 1;
+        int pnum = 1;
+        int divisor = 1234567;
+
+        if(n == 2) return 1;
+
+        for(int i = 3; i <= n; i++) {
+            answer = (ppnum + pnum) % divisor;
+            ppnum = pnum;
+            pnum = answer;
+        }
+
+        return answer;
+    }
+}
