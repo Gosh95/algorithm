be261605ed8a2fc7ad42ef3c6660bef3f1188044
Gosh95 <pocal777@naver.com>
2021-12-09 08:09:50 +0300

    9498 시험 성적
    https://www.acmicpc.net/problem/9498

---
+++ src/test/baekjoon/if문/TestScore.java
@@ -0,0 +1,23 @@
+package test.baekjoon.if문;
+
+import java.util.Scanner;
+
+public class TestScore {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int input = scanner.nextInt();
+
+        if(input >= 90 && input <= 100) {
+            System.out.println("A");
+        } else if(input >= 80 && input <= 89) {
+            System.out.println("B");
+        } else if (input >= 70 && input <= 79) {
+            System.out.println("C");
+        } else if(input >= 60 && input <= 69) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
+}
