390d955b5ce918e0fbc23bb2e91982bff095a99f
Gosh95 <pocal777@naver.com>
2021-12-09 08:21:16 +0300

    2884 알람 시계
    https://www.acmicpc.net/problem/2884

---
+++ src/test/baekjoon/if문/Alarm.java
@@ -0,0 +1,25 @@
+package test.baekjoon.if문;
+
+import java.util.Scanner;
+
+public class Alarm {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int H = in.nextInt();
+        int M = in.nextInt();
+
+        if(M < 45) {
+            M = 60 - (45 - M);
+            H -= 1;
+
+            if(H < 0) {
+                H = 23;
+            }
+
+            System.out.println(H + " " + M);
+        } else {
+            System.out.println(H + " " + (M - 45));
+        }
+    }
+}
