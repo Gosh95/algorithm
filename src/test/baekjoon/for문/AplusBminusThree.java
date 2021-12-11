67308f531b1f882374fe9625e4c8472dd10d9d61
Gosh95 <pocal777@naver.com>
2021-12-11 08:22:50 +0300

    10950 A + B - 3
    https://www.acmicpc.net/problem/10950

---
+++ src/test/baekjoon/for문/AplusBminusThree.java
@@ -0,0 +1,23 @@
+package test.baekjoon.for문;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AplusBminusThree {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        int A;
+        int B;
+
+        for(int i = 0; i < T; i++) {
+            String[] str = br.readLine().split(" ");
+            A = Integer.parseInt(str[0]);
+            B = Integer.parseInt(str[1]);
+
+            System.out.println(A + B);
+        }
+    }
+}
