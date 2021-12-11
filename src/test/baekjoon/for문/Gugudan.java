a60514a750a9852ab5e6f75ef85037f1c08f0dfa
Gosh95 <pocal777@naver.com>
2021-12-11 08:22:26 +0300

    2739 구구단
    https://www.acmicpc.net/problem/2739

---
+++ src/test/baekjoon/for문/Gugudan.java
@@ -0,0 +1,18 @@
+package test.baekjoon.for문;
+
+import java.util.Scanner;
+
+public class Gugudan {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int n = scanner.nextInt();
+
+        for(int i = 1; i <= 9; i++) {
+            sb.append(n + " * " + i + " = " + n * i).append('\n');
+        }
+
+        System.out.println(sb.toString());
+    }
+}
