c9eb59a1e953b93ec0d88ad73b124cd932976832
Gosh95 <pocal777@naver.com>
2022-01-24 12:38:02 +0900

    이것이 취업을 위한 코딩테스트다. 예제 3-1
    거스름돈

---
+++ src/test/baekjoon/thisiscodingtest/greedy/Change.java
@@ -0,0 +1,20 @@
+package test.baekjoon.thisiscodingtest.greedy;
+
+import java.io.*;
+
+public class Change {
+    public static void main(String[] args) throws IOException {
+        int[] money = new int[]{500, 100, 50, 10};
+        int answer = 0;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer N = Integer.valueOf(br.readLine());
+
+        for(Integer i : money) {
+            answer += N / i;
+            N = N % i;
+        }
+
+        System.out.println(answer);
+    }
+}
