edffbb58faba6a58491b258b01efcae4b600b503
Gosh95 <pocal777@naver.com>
2022-01-24 15:28:10 +0900

    이것이 취업을 위한 코딩테스트다. 그리디 4-1이 될 때까지
    https://velog.io/@goshk95/%EC%9D%B4%EA%B2%83%EC%9D%B4-%EC%B7%A8%EC%97%85%EC%9D%84-%EC%9C%84%ED%95%9C-%EC%BD%94%EB%94%A9%ED%85%8C%EC%8A%A4%ED%8A%B8%EB%8B%A4.-%EA%B7%B8%EB%A6%AC%EB%94%94-4-1%EC%9D%B4-%EB%90%A0-%EB%95%8C%EA%B9%8C%EC%A7%80

---
+++ src/test/baekjoon/thisiscodingtest/greedy/UntilItBecomeOne.java
@@ -0,0 +1,29 @@
+package test.baekjoon.thisiscodingtest.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class UntilItBecomeOne {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String[] split = str.split(" ");
+        int count = 0;
+
+        int N = Integer.parseInt(split[0]);
+        int K = Integer.parseInt(split[1]);
+
+        while(N != 1) {
+            if(N % K == 0) {
+                N = N / K;
+                count++;
+            } else {
+                N--;
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
