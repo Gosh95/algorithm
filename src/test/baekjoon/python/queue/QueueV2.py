dd23ad062217cb14a7fe266b986bd161d82ce03d
goshk95 <goshk1995@gmail.com>
2022-02-07 13:31:56 +0900

    [백준] 큐 2 18258번 - Python
    https://velog.io/@goshk95/%EB%B0%B1%EC%A4%80-%ED%81%90-2-18258%EB%B2%88-Python

---
+++ src/test/baekjoon/python/queue/QueueV2.py
@@ -0,0 +1,24 @@
+from collections import deque
+import sys
+
+N = int(input())
+queue = deque()
+for i in range(N):
+    command = list(sys.stdin.readline().split())
+    size = len(queue)
+    if command[0].__contains__("push"):
+        queue.append(int(command[1]))
+    elif command[0] == "pop":
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    elif command[0] == "size":
+        print(size)
+    elif command[0] == "empty":
+        print(0) if size != 0 else print(1)
+    elif command[0] == "front":
+        print(queue[0]) if queue else print(-1)
+    elif command[0] == "back":
+        print(queue[-1]) if queue else print(-1)
+
