1755a4c40baf598996bf2987bd69bad25c5cab1f
Gosh95 <pocal777@naver.com>
2022-02-09 02:01:00 +0900

    [백준] 랜선 자르기 1654번 - Python

--- src/test/baekjoon/python/binarysearch/CutLanCable.py
+++ src/test/baekjoon/python/binarysearch/CutLanCable.py
@@ -1,1 +1,18 @@
-N = int(input())
+import sys
+N, K = map(int, sys.stdin.readline().split())
+nums = []
+for i in range(N):
+    nums.append(int(sys.stdin.readline()))
+def binary_search(start, end):
+    while start <= end:
+        total = 0
+        mid = (start + end) // 2
+        for x in nums:
+            total += x // mid
+        if total >= K:
+            start = mid + 1
+        elif total < K:
+            end = mid - 1
+    return end
+
+print(binary_search(1, max(nums)))
