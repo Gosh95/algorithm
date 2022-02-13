8a1aca9e35416d85d57826b2a6651f3ae2e553ee
Gosh95 <pocal777@naver.com>
2022-02-14 01:29:10 +0900

    [백준] 회의실 배정 1931번 - Java

---
+++ src/test/baekjoon/java/greedy/MeetingRoomAssignment.java
@@ -0,0 +1,44 @@
+package test.baekjoon.java.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class MeetingRoomAssignment {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[][] time = new int[N][2];
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            time[i][0] = Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(time, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                } else {
+                    return o1[1] - o2[1];
+                }
+            }
+        });
+
+        int lastTime = time[0][1];
+        int count = 1;
+        for(int i = 1; i < N; i++) {
+            if(lastTime <= time[i][0]) {
+                lastTime = time[i][1];
+                count += 1;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
