90bc5af24c570317686d1ac2c6321c5d6a4dab0d
Gosh95 <pocal777@naver.com>
2022-02-18 04:38:38 +0900

    [백준] 미로 탐색 2178번 - Java

---
+++ src/test/baekjoon/java/dfsbfs/MazeExploration.java
@@ -0,0 +1,79 @@
+package test.baekjoon.java.dfsbfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int x;
+    int y;
+
+    Node (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class MazeExploration {
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int[][] graph = new int[100][100];
+    static boolean[][] visited = new boolean[100][100];
+    static int N;
+    static int M;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] items = br.readLine().split(" ");
+        N = Integer.parseInt(items[0]);
+        M = Integer.parseInt(items[1]);
+
+        for(int i = 0; i < N; i++) {
+            String[] values = br.readLine().split("");
+            for(int j = 0; j < M; j++) {
+                graph[i][j] = Integer.parseInt(values[j]);
+            }
+        }
+
+
+
+        System.out.println(bfs(0, 0));
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                System.out.print(graph[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    static private int bfs(int x, int y) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(new Node(x, y));
+        visited[x][y] = true;
+
+        while(!queue.isEmpty()) {
+            Node node = queue.poll();
+            for(int i = 0; i < 4; i++) {
+                int nx = node.x + dx[i];
+                int ny = node.y + dy[i];
+
+                if(nx < 0 || ny < 0 || nx >= N || ny >= M) {
+                    continue;
+                }
+
+                if(graph[nx][ny] == 0) {
+                    continue;
+                }
+
+                if(!visited[nx][ny] && graph[nx][ny] == 1) {
+                    graph[nx][ny] = graph[node.x][node.y] + 1;
+                    visited[nx][ny] = true;
+                    queue.offer(new Node(nx, ny));
+                }
+            }
+        }
+
+        return graph[N - 1][M - 1];
+    }
+}
