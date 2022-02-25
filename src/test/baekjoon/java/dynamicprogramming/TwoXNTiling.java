0e65287b52706178a5805cad5008fc8308a11b37
Gosh95 <pocal777@naver.com>
2022-02-26 04:14:05 +0900

    [백준] 2xn 타일링 11726번 - [Java/Python]

---
+++ src/test/baekjoon/java/dynamicprogramming/TwoXNTiling.java
@@ -0,0 +1,21 @@
+package test.baekjoon.java.dynamicprogramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TwoXNTiling {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] d = new int[1001];
+        d[1] = 1;
+        d[2] = 2;
+
+        for(int i = 3; i < n + 1; i++) {
+            d[i] = (d[i - 1] + d[i - 2]) % 10007;
+        }
+
+        System.out.println(d[n]);
+    }
+}
