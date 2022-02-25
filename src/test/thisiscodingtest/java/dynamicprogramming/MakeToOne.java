b6ceab385c0a5d81c01462f2b9ba02439026f605
Gosh95 <pocal777@naver.com>
2022-02-25 16:49:36 +0900

    이것이 취업을 위한 코딩 테스트다. 다이나믹 프로그래밍 [1로 만들기]

---
+++ src/test/thisiscodingtest/java/dynamicprogramming/MakeToOne.java
@@ -0,0 +1,31 @@
+package test.thisiscodingtest.java.dynamicprogramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MakeToOne {
+    static int[] d = new int[30001];
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        System.out.println(dynamicProgramming(N));
+    }
+    static int dynamicProgramming(int n) {
+        for(int i = 2; i < n + 1; i++) {
+            d[i] = d[i - 1] + 1;
+
+            if(i % 2 == 0) {
+                d[i] = Math.min(d[i], d[i / 2] + 1);
+            } else if(i % 3 == 0) {
+                d[i] = Math.min(d[i], d[i / 3] + 1);
+            } else if(i % 5 == 0) {
+                d[i] = Math.min(d[i], d[i / 5] + 1);
+            }
+        }
+
+        return d[n];
+    }
+}
