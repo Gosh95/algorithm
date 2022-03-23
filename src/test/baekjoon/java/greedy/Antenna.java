b9d1a68d519cb288807a74c758c660bd1ad444c7
Gosh95 <pocal777@naver.com>
2022-03-23 15:45:18 +0900

    [백준] 안테나 18310번 - [Java/Python]

---
+++ src/test/baekjoon/java/greedy/Antenna.java
@@ -0,0 +1,23 @@
+package test.baekjoon.java.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Antenna {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] position = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            position[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(position);
+
+        System.out.println(position[(N - 1) / 2]);
+    }
+}
---
+++ src/test/baekjoon/python/greedy/Antenna.py
@@ -0,0 +1,4 @@
+N = int(input())
+position = list(map(int, input().split()))
+position.sort()
+print(position[(N - 1) // 2])
