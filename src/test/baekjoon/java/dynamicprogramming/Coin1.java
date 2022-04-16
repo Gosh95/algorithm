ddf9b299552164f4b7d9b806b7c28afdec974db2
Gosh95 <pocal777@naver.com>
2022-04-16 14:38:24 +0900

    [백준] 동전1 2293번 - [Java/Python]

---
+++ src/test/baekjoon/java/dynamicprogramming/Coin1.java
@@ -0,0 +1,32 @@
+package test.baekjoon.java.dynamicprogramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Coin1 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[n];
+        for(int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[10001];
+        dp[0] = 1;
+
+        for(int coin : coins) {
+            for(int i = coin; i < k + 1; i++) {
+                dp[i] = dp[i - coin] + dp[i];
+            }
+        }
+
+        System.out.println(dp[k]);
+    }
+}
